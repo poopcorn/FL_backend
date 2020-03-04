155efb9cabca54a7bf2f92ff2206923868438379
poopcorn <458892325@qq.com>
2020-03-04 15:55:07 +0800

    add host

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '5p^di)xy=0p)lmhkt+l)(ud(=a4fw^m38x03fuomkq6l$clj)v'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.127']
 
 
 # Application definition
