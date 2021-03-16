42a4bd26ac72163841df202231e68a308267b1d8
poopcorn <458892325@qq.com>
2021-03-16 18:14:25 +0800

    debug

--- const.py
+++ const.py
@@ -1,14 +1,15 @@
 # 本次模型和数据集对应的全局数据
-DEFAULT_MODEL = 'EasyCNN'
-DEFAULT_DATASET = 'FEMINIST'
+# DEFAULT_MODEL = 'EasyCNN'
+# DEFAULT_DATASET = 'FEMINIST'
 
 DEFAULT_MODEL = 'LeNet'
-DEFAULT_DATASET = 'DIGIT5'
+# DEFAULT_DATASET = 'DIGIT5'
+DEFAULT_DATASET = 'FEMINIST'
 DATA_SAVE_FILE = 'data/{}_{}'.format(DEFAULT_MODEL, DEFAULT_DATASET)
-DEFAULT_CLIENT_NUM = 4
-DEFAULT_ROUND_NUM = 100
+DEFAULT_CLIENT_NUM = 35
+DEFAULT_ROUND_NUM = 150
 LAYERS_NANME = ['layer1', 'layer2']
 ROUND_EVERY_FILE = 10
 
 # 模型计算数据保存目录
-JSON_PATH = '/home/zty_11621014/fedcare/federated/data/experiments/case2/'
+JSON_PATH = '/home/zty_11621014/fedcare/federated/data/experiments/case1/'
